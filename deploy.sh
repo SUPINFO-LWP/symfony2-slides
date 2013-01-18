1ab0fb5c79fce5a47df1bad64b00464df2edee57
Adrien Brault <adrien.brault@gmail.com>
2013-01-18 16:28:22 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git checkout --orphan gh-pages
+git rm -r --cached .
+find . -name "*.cfg" -exec landslide {} \;
+git add -f *.html
+git commit -m "Deploy"
+git push origin gh-pages -f
+git clean -fd
+git checkout master
+git branch -D gh-pages
